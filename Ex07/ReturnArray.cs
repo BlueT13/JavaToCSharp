30504459f0e5940aaac1d2e109fed7d2907173e5
DragonT13 <alex23456@naver.com>
2023-01-07 20:58:31 +0900

    Ex07의 ReturnArray까지 완료

---
+++ Ex07/ReturnArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex07
+{
+    class ReturnArray
+    {
+        int[] makeArray()
+        {
+            int[] temp = new int[4];
+            for(int i=0;i<temp.Length;i++)
+            {
+                temp[i] = i;
+            }
+            return temp;
+        }
+
+        public void PrintReturnArray()
+        {
+            int[] intArray;
+            intArray= makeArray();
+            for(int i = 0; i<intArray.Length; i++)
+                Console.Write(intArray[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
