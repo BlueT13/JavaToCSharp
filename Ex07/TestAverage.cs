be97ddf3cb2dc261f6960d1fd728eec0ff6df01c
DragonT13 <alex23456@naver.com>
2023-01-11 21:05:05 +0900

    Ex07/TestAverage 수정(메소드 사용)
    - getAverage 메소드 구현
    - getTestAverage 메소드 구현

--- Ex07/TestAverage.cs
+++ Ex07/TestAverage.cs
@@ -17,33 +17,43 @@ namespace Ex07
                               { 91, 94, 100 },
                               { 76, 72, 84 },
                               { 87, 93, 73 } };
-            double sum = 0;
-            double sum2 = 0;
-
+            
             // score출력
-            for(int i =0; i< score.GetLength(0); i++)
+            for (int i =0; i< score.GetLength(0); i++)
             {
                 for(int j = 0; j < score.GetLength(1); j++)
                 {
                     Console.Write("{0,3}\t", score[i, j]);
-                    sum += score[i, j];
                 }
                 // score 평균 출력
-                Console.Write("{0:N1}", sum / 3);
-                sum = 0;
+                getAverage(score,i);
                 Console.WriteLine();
             }
-
             // test 평균 출력
-            for(int m = 0; m < score.GetLength(1); m++)
+            for(int j = 0; j<score.GetLength(1); j++)
+                getTestAverage(score, j);
+        }
+
+        // score 평균 계산 함수
+        void getAverage(int[,] arr, int i)
+        {
+            double sum = 0;
+            for(int k = 0; k<arr.GetLength(1); k++)
             {
-                for (int k = 0; k < score.GetLength(0); k++)
-                {
-                    sum2 += score[k, m];
-                }
-                Console.Write("{0:N1}\t", sum2 / score.GetLength(0));
-                sum2 = 0;
+                sum += arr[i, k];
             }
+            Console.Write("{0:N1}", sum / arr.GetLength(1));
+        }
+
+        // test 평균 계산 함수
+        void getTestAverage(int[,] arr, int j)
+        {
+            double sum = 0;
+            for(int l = 0; l<arr.GetLength(0); l++)
+            {
+                sum += arr[l, j];
+            }
+            Console.Write("{0:N1}\t", sum / arr.GetLength(0));
         }
     }
 }
