c102a19baaa20e09fad7055c44016c8649a2f6db
DragonT13 <alex23456@naver.com>
2023-01-12 18:18:48 +0900

    Ex07/Ex07_1.cs refactoring

--- Ex07/Ex07_1.cs
+++ Ex07/Ex07_1.cs
@@ -1,61 +1,79 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace Ex07
 {
-    internal class Ex07_1
-    {
-        public void PrintEx07_1()
-        {
-            int[,] parity = { { 1, 0, 1, 0, 1, 1, 1, 1 }, 
-                              { 1, 0, 0, 0, 0, 0, 1, 1 }, 
-                              { 0, 1, 0, 0, 0, 0, 0, 0 }, 
-                              { 1, 1, 1, 1, 0, 0, 0, 0 }, 
-                              { 1, 0, 1, 1, 1, 0, 0, 1 }, 
-                              { 0, 0, 0, 0, 0, 1, 1, 1 }, 
-                              { 1, 1, 1, 1, 1, 1, 1, 1 }, 
-                              { 0, 1, 1, 1, 1, 0, 0, 0 } };
-            int sum = 0;
-            int sum2 = 0;
-            int count = 0;
-            
-            // 세로 방향 패리티 비트 출력
-            for(int i=0;i<parity.GetLength(0);i++)
-            {
-                for(int j=0;j<parity.GetLength(1);j++)
-                {
-                    Console.Write(parity[i,j]+" ");
-                    sum += parity[i, j];
-                }
-                if (sum % 2 == 0)
-                    Console.Write("0");
-                else
-                {
-                    Console.Write("1");
-                    count++;
-                }
-                sum = 0;
-                Console.WriteLine();
-            }
+	internal class Ex07_1
+	{
+		public void PrintEx07_1()
+		{
+			int[,] inputBits = { { 1, 0, 1, 0, 1, 1, 1, 1 },
+								 { 1, 0, 0, 0, 0, 0, 1, 1 },
+								 { 0, 1, 0, 0, 0, 0, 0, 0 },
+								 { 1, 1, 1, 1, 0, 0, 0, 0 },
+								 { 1, 0, 1, 1, 1, 0, 0, 1 },
+								 { 0, 0, 0, 0, 0, 1, 1, 1 },
+								 { 1, 1, 1, 1, 1, 1, 1, 1 },
+								 { 0, 1, 1, 1, 1, 0, 0, 0 } };
 
-            // 가로 방향 패리티 비트 출력
-            for(int j=0; j<parity.GetLength(1);j++)
-            {
-                for (int i = 0; i < parity.GetLength(0); i++)
-                {
-                    sum2 += parity[i, j];
-                }
-                if (sum2 % 2 == 0)
-                    Console.Write("0 ");
-                else
-                    Console.Write("1 ");
-                sum2 = 0;
-            }
+			int[] colParity = new int[inputBits.GetLength(0)];
+			for (int i = 0; i < inputBits.GetLength(0); i++)
+			{
+				int sum = 0;
+				for (int j = 0; j < inputBits.GetLength(1); j++)
+				{
+					sum += inputBits[i, j];
+				}
+				EvenCheck(colParity, sum, i);
+			}
 
-            // 마지막 행과 열의 원소
-            if (count % 2 == 0)
-                Console.Write("0");
-            else
-                Console.Write("1");
-        }
-    }
+			int[] rowParity = new int[inputBits.GetLength(1)];
+			for (int j = 0; j < inputBits.GetLength(1); j++)
+			{
+				int sum = 0;
+				for (int i = 0; i < inputBits.GetLength(0); i++)
+				{
+					sum += inputBits[i, j];
+				}
+				EvenCheck(rowParity, sum, j);
+			}
+
+			for (int i = 0; i < inputBits.GetLength(0); i++)
+			{
+				for (int j = 0; j < inputBits.GetLength(1); j++)
+				{
+					Console.Write("{0} ", inputBits[i, j]);
+				}
+				Console.Write(colParity[i]);
+				Console.WriteLine();
+			}
+
+			int rowParitySum = 0;
+			for (int i = 0; i < rowParity.Length; i++)
+			{
+				rowParitySum += rowParity[i];
+				Console.Write("{0} ", rowParity[i]);
+			}
+			if (rowParitySum % 2 == 0)
+			{
+				Console.Write("0");
+			}
+			else
+			{
+				Console.Write("1");
+			}
+		}
+
+		private int EvenCheck(int[] arr, int sum, int n)
+		{
+			if (sum % 2 == 0)
+			{
+				return arr[n] = 0;
+			}
+			else
+			{
+				return arr[n] = 1;
+			}
+		}
+	}
 }
