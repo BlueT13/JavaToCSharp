d1540ca2e24c59257c6533d99a4374517ba24093
DragonT13 <alex23456@naver.com>
2023-01-13 17:38:05 +0900

    Ex09/Circle까지 완료

---
+++ Ex09/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ex09
+{
+	internal class Circle
+	{
+		int radius;
+		string name;
+
+		public Circle() { }
+
+		public double GetArea()
+		{
+			return 3.14 * radius * radius;
+		}
+
+		public void PrintCircle()
+		{
+			Circle pizza;
+			pizza = new Circle();
+			pizza.radius = 10;
+			pizza.name = "CSharp-Pizza";
+			double area = pizza.GetArea();
+			Console.WriteLine("Area of {0}: {1:N1}", pizza.name, area);
+
+			Circle donut = new Circle();
+			donut.radius = 2;
+			donut.name = "CSharp-Donut";
+			area = donut.GetArea();
+			Console.WriteLine("Area of {0}: {1:N2}", donut.name, area);
+		}
+	}
+}
---
+++ Ex09/MainClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ex09
+{
+	internal class MainClass
+	{
+		static void Main()
+		{
+			Circle circle = new Circle();
+			circle.PrintCircle();
+		}
+	}
+}
