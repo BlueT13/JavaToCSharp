b6ce03fb2e7c9ec278cec12878e048009f81a74b
DragonT13 <alex23456@naver.com>
2023-01-18 15:55:30 +0900

    Ex10/AlarmTime.cs 진행 중

---
+++ Ex10/AlarmTime.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ex10
+{
+	internal class AlarmTime
+	{
+		int hour;
+		int minute;
+		bool active = false;
+
+		public AlarmTime(int hour, int minute) : this(hour, minute, false) { }
+
+		public AlarmTime(int hour, int minute, bool active)
+		{
+			this.hour = hour;
+			this.minute = minute;
+			this.active = active;
+		}
+
+		public bool IsActive()
+		{
+			return true;
+		}
+
+		public int GetHour()
+		{
+			return hour;
+		}
+
+		public void SetHour(int hour)
+		{
+
+		}
+
+		public int GetMinute()
+		{
+			return minute;
+		}
+
+		public void SetMinute(int minute)
+		{
+
+		}
+	}
+
+
+}
