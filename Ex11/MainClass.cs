b5ddfd5d9188e0de1fffca3ca7ed9c757929968d
DragonT13 <alex23456@naver.com>
2023-01-20 21:19:17 +0900

    Ex11/ValuePassing.cs까지 완료

---
+++ Ex11/MainClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex11
+{
+	internal class MainClass
+	{
+		private static void Main()
+		{
+			ValuePassing.PrintValue();
+		}
+	}
+}
---
+++ Ex11/ValuePassing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex11
+{
+	internal class ValuePassing
+	{
+		public static void PrintValue()
+		{
+			int n = 10;
+			Increase(n);
+			Console.WriteLine(n);
+		}
+		static void Increase(int m)
+		{
+			m = m + 1;
+		}
+	}
+}
