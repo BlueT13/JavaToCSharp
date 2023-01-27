b9b54a6a26d067a7105487bafba78a772e6a7d1f
DragonT13 <alex23456@naver.com>
2023-01-27 11:12:09 +0900

    Ex12/Ex.cs 수정

--- Ex12/Ex.cs
+++ Ex12/Ex.cs
@@ -10,11 +10,13 @@ namespace Ex12
 			s1 = new StaticSample();
 			s1.n = 5;
 			s1.g();
+			//오류
 			//s1.m = 50;
 			//Console.WriteLine(s1.m);
 			s2 = new StaticSample();
 			s2.n = 8;
 			s2.h();
+			//오류
 			//s2.f();
 			//Console.WriteLine(s2.m);
 		}
