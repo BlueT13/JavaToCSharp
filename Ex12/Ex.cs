9070308ebc013c5b1b894b4bdca72da25025b4ef
DragonT13 <alex23456@naver.com>
2023-01-28 23:42:06 +0900

    Ex12/Ex.cs 수정
     - 클래스 이름을 통한 static 멤버 사용

--- Ex12/Ex.cs
+++ Ex12/Ex.cs
@@ -6,19 +6,15 @@ namespace Ex12
 	{
 		public static void StaticSampleMain()
 		{
-			StaticSample s1, s2;
+			StaticSample.m = 10;
+			Console.WriteLine(StaticSample.m);
+
+			StaticSample.f();
+			Console.WriteLine(StaticSample.m);
+
+			StaticSample s1;
 			s1 = new StaticSample();
-			s1.n = 5;
-			s1.g();
-			//오류
-			//s1.m = 50;
-			//Console.WriteLine(s1.m);
-			s2 = new StaticSample();
-			s2.n = 8;
-			s2.h();
-			//오류
-			//s2.f();
-			//Console.WriteLine(s2.m);
+			Console.WriteLine(StaticSample.m);
 		}
 	}
 }
