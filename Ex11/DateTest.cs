5e6e1859f5c4665426affbe701a6f16af65ceebb
DragonT13 <alex23456@naver.com>
2023-01-29 19:55:09 +0900

    Ex11/DateTest.cs Increase()메소드 구현 완료

--- Ex11/DateTest.cs
+++ Ex11/DateTest.cs
@@ -37,7 +37,7 @@ namespace Ex11
 				return testDay;
 			}
 			else
-				return 1;	
+				return 1;
 		}
 
 		public string ToString()
@@ -47,10 +47,26 @@ namespace Ex11
 
 		public void Increase()
 		{
-			day++;
-			for(int i =0; i< month; i++)
+			int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+			if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
 			{
-
+				daysPerMonth[2] = 29;
+			}
+			
+			if (day < daysPerMonth[month] && month < 13)
+			{
+				day++;
+			}
+			else if (day == daysPerMonth[month] && month < 12)
+			{
+				day = 1;
+				month++;
+			}
+			else if (day == daysPerMonth[month] && month == 12)
+			{
+				year++;
+				day = 1;
+				month = 1;
 			}
 		}
 	}
@@ -61,7 +77,7 @@ namespace Ex11
 			Date date = new Date(1, 1, 2020);
 			Console.WriteLine("Graduation date: " + date.ToString());
 
-			for(int  i = 0; i < 800; i++) 
+			for (int i = 0; i < 1827; i++)
 			{
 				date.Increase();
 				Console.WriteLine(date.ToString());
