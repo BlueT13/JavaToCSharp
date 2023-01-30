9f17c064be561137a27dcb7d7c10f34ae06a6957
DragonT13 <alex23456@naver.com>
2023-01-30 17:36:14 +0900

    Ex11/DateTest.cs refactoring

--- Ex11/DateTest.cs
+++ Ex11/DateTest.cs
@@ -2,74 +2,6 @@
 
 namespace Ex11
 {
-	class Date
-	{
-		private int day;
-		private int month;
-		private int year;
-
-		public Date(int month, int day, int year)
-		{
-			this.month = CheckMonth(month);
-			this.year = year;
-			this.day = CheckDay(day);
-		}
-
-		private int CheckMonth(int testMonth)
-		{
-			if (testMonth > 0 && testMonth <= 12)
-			{
-				return testMonth;
-			}
-			else
-				return 1;
-		}
-
-		private int CheckDay(int testDay)
-		{
-			int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-			if (testDay > 0 && testDay <= daysPerMonth[month])
-			{
-				return testDay;
-			}
-			else if (month == 2 && testDay == 29 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))
-			{
-				return testDay;
-			}
-			else
-				return 1;
-		}
-
-		public string ToString()
-		{
-			return month + "/" + day + "/" + year;
-		}
-
-		public void Increase()
-		{
-			int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-			if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-			{
-				daysPerMonth[2] = 29;
-			}
-			
-			if (day < daysPerMonth[month] && month < 13)
-			{
-				day++;
-			}
-			else if (day == daysPerMonth[month] && month < 12)
-			{
-				day = 1;
-				month++;
-			}
-			else if (day == daysPerMonth[month] && month == 12)
-			{
-				year++;
-				day = 1;
-				month = 1;
-			}
-		}
-	}
 	internal class DateTest
 	{
 		public static void PrintDateTest()
@@ -81,6 +13,83 @@ namespace Ex11
 			{
 				date.Increase();
 				Console.WriteLine(date.ToString());
+			}
+		}
+	}
+
+	class Date
+	{
+		private static readonly int[] kDaysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		private static readonly int[] kLeapDaysPerMonth = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		private int day;
+		private int month;
+		private int year;
+
+		public Date(int day, int month, int year)
+		{
+			if (CheckDate(day, month, year))
+			{
+				this.day = day;
+				this.month = month;
+				this.year = year;
+			}
+			else
+			{
+				this.day = -1;
+				this.month = -1;
+				this.year = -1;
+			}
+		}
+
+		private static bool CheckDate(int day, int month, int year)
+		{
+			if (month < 1 || month > 12)
+			{
+				return false;
+			}
+
+			int[] daysPerMonth = IsLeapYear(year) ? kLeapDaysPerMonth : kDaysPerMonth;
+			if (day < 1 || day > daysPerMonth[month])
+			{
+				return false;
+			}
+			return true;
+		}
+
+		private static bool IsLeapYear(int year)
+		{
+			return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+		}
+
+		public string ToString()
+		{
+			return month + "/" + day + "/" + year;
+		}
+
+		public void Increase()
+		{
+			int[] daysPerMonth = kDaysPerMonth;
+
+			if (IsLeapYear(year))
+			{
+				daysPerMonth = kLeapDaysPerMonth;
+			}
+
+			if (day < daysPerMonth[month] && month < 13)
+			{
+				day++;
+			}
+			else if (day == daysPerMonth[month] && month < 12)
+			{
+				day = 1;
+				month++;
+			}
+			else if (day == daysPerMonth[month] && month == 12)
+			{
+				day = 1;
+				month = 1;
+				year++;
 			}
 		}
 	}
