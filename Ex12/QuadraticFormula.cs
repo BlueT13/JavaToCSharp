114de350070dc80f742470e71dc31625e5da7723
DragonT13 <alex23456@naver.com>
2023-01-31 18:42:35 +0900

    Ex12완료
     - Ex12/QuadraticFormula.cs refactoring 완료

---
+++ Ex12/QuadraticFormula.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ex12
+{
+	public class QuadraticFormula
+	{
+		public static double[] Calculate(double a, double b, double c)
+		{
+			double[] x;
+			if (Math.Pow(b, 2) - 4 * a * c < 0)
+			{
+				x = new double[0];
+			}
+			else if (Math.Pow(b, 2) - 4 * a * c == 0)
+			{
+				x = new double[1];
+				x[0] = -b / (2 * a);
+			}
+			else
+			{
+				x = new double[2];
+				x[0] = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+				x[1] = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+			}
+			return x;
+		}
+		
+		public static void PrintAnswer(double[] x)
+		{
+			if (x.Length == 0)
+			{
+				Console.WriteLine("no answer");
+			}
+			else
+			{
+				Console.Write("answer: ");
+				for (int i = 0; i < x.Length; i++)
+				{
+					Console.Write(x[i]+" ");
+				}
+			}
+		}
+	}
+}
---
+++ Ex12/QuadraticFormulaMainClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex12
+{
+	internal class QuadraticFormulaMainClass
+	{
+		public static void Answer()
+		{
+			double a, b, c;
+			Console.WriteLine("2차 방정식 ax^2 + bx + c의 실수해 구하기");
+			Console.Write("a값 입력: ");
+			a = Convert.ToDouble(Console.ReadLine());
+			Console.Write("b값 입력: ");
+			b = Convert.ToDouble(Console.ReadLine());
+			Console.Write("c값 입력: ");
+			c = Convert.ToDouble(Console.ReadLine());
+			
+			double[] x = QuadraticFormula.Calculate(a, b, c);
+			QuadraticFormula.PrintAnswer(x);
+		}
+	}
+}
