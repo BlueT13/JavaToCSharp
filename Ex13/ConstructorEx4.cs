de9b945d2d47026ef7f5e009759df525364e7d10
DragonT13 <alex23456@naver.com>
2023-02-04 23:46:36 +0900

    Ex13/ConstructorEx4.cs 수정

--- Ex13/ConstructorEx4.cs
+++ Ex13/ConstructorEx4.cs
@@ -5,25 +5,25 @@ namespace Ex13_1
 {
 	class A
 	{
-		public A() 
+		public A()
 		{
 			Console.WriteLine("A");
 		}
 		public A(int x)
 		{
-			Console.WriteLine("A"+x);
+			Console.WriteLine("A" + x);
 		}
 	}
 
-	class B:A
+	class B : A
 	{
 		public B()
 		{
 			Console.WriteLine("B");
 		}
-		public B(int x):base(5)
+		public B(int x) : base(x)
 		{
-			Console.WriteLine("B"+x);
+			Console.WriteLine("B" + x);
 		}
 	}
 	internal class ConstructorEx4
