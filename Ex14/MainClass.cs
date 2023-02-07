a719db1409e4fa23cf6bbed7a978536fa70f4486
DragonT13 <alex23456@naver.com>
2023-02-07 22:10:52 +0900

    Ex14/UpCastingEx.cs까지 완료

---
+++ Ex14/MainClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex14
+{
+	internal class MainClass
+	{
+		public static void Main()
+		{
+			UpCastingEx.PrintUpCastingEx();
+		}
+	}
+}
---
+++ Ex14/UpCastingEx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex14
+{
+	class Person
+	{
+		public string name;
+		public string id;
+
+		public Person(string name)
+		{
+			this.name = name;
+		}
+	}
+
+	class Student : Person
+	{
+		public string grade;
+		public string department;
+
+		public Student(string name) : base(name) { }
+	}
+
+	internal class UpCastingEx
+	{
+		public static void PrintUpCastingEx()
+		{
+			Person p;
+			Student s = new Student("James");
+			p = s;
+
+			Console.WriteLine(p.name);
+			//p.grade = "A";		//error
+			//p.department = "SW";	//error
+		}
+	}
+}
