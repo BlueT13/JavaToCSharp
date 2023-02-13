c99bcb046a33506656254b0a87fc3b51325aca83
DragonT13 <alex23456@naver.com>
2023-02-13 18:25:29 +0900

    Ex14/UsingOverride.cs refactoring

--- Ex14/UsingOverride.cs
+++ Ex14/UsingOverride.cs
@@ -6,11 +6,20 @@ namespace Ex14
 	{
 		public static void PrintUsingOverride()
 		{
-			Shape start, last, obj;
-			obj = new Line(); start = obj; last = obj;
-			obj = new Rect(); last.next = obj; last = obj;
-			obj = new Line(); last.next = obj; last = obj;
-			obj = new Circle(); last.next = obj; last = obj;
+			//Shape start, last, obj;
+			//obj = new Line(); start = obj; last = obj;
+			//obj = new Rect(); last.next = obj; last = obj;
+			//obj = new Line(); last.next = obj; last = obj;
+			//obj = new Circle(); last.next = obj; last = obj;
+
+			Shape start, last;
+			start = new Line();
+			last = start;
+			last.next = new Rect();
+			last = last.next;
+			last.next = new Line();
+			last = last.next;
+			last.next = new Circle();
 
 			Shape p = start;
 			while (p != null)
